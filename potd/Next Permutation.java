a662c206e76e7f31386dbf0ef9e20b93d6101a9a
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-11-19 23:52:02 +0530

    Microsoft
    https://www.geeksforgeeks.org/problems/next-permutation5226/1

---
+++ potd/Next Permutation.java
@@ -0,0 +1,49 @@
+Date: 19.11.24
+
+class Solution {
+    void nextPermutation(int[] arr) {
+        int ind =-1;
+        int n = arr.length;
+        
+        
+        //loop to find out the break point 
+        for(int i= n-2; i>=0; i--){
+            if(arr[i]<arr[i+1]){
+                ind =i;
+                break;
+            }
+            
+        }
+        
+        if(ind ==-1){
+            reverse(arr, 0, n - 1);;
+            return;
+        }
+        
+        // loop to swap values with the next largest
+        
+        for(int i = n-1; i>ind; i--){
+            if(arr[i]>arr[ind]){
+                swap(arr, i, ind);
+                break;
+            }
+        }
+        reverse(arr, ind + 1, n - 1);
+        
+    }
+    public void swap(int []arr, int i, int ind){
+        int temp = arr[ind];
+        arr[ind] = arr[i];
+        arr[i] = temp;
+        
+    }
+    public void reverse(int []arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end] = temp;
+            end--;
+            start++;
+        }
+    }
+}
