6095f1044ba8b7b8c3ac117af815e451da5ce5ad
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-11-19 23:52:49 +0530

    Amazon microsoft

--- potd/Square root of a number [easy not so easy].java
+++ potd/Square root of a number [easy not so easy].java
@@ -4,7 +4,7 @@ class Solution {
     long floorSqrt(long n) {
         if(n==1) return 1;
         long start =0;
-        long end = n/2;
+        long end = n/2; 
         while(start<=end){
             long mid = start+(end-start)/2;
             if(mid*mid==n){
