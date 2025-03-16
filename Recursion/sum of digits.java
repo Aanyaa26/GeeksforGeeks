531b7a0dd8ee2ef6ac93542cc63804a642293a20
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-03-16 13:12:20 +0530

    basic recursion

--- Recursion/sum of digits.java
+++ Recursion/sum of digits.java
@@ -11,3 +11,13 @@ Date: 16.03.25
         return ans;
     }
 }
+
+
+------Recursion------------
+class Solution {
+    static int sumOfDigits(int n) {
+        if(n==0)return 0;
+        
+        return (n%10)+sumOfDigits(n/10);
+    }
+}
